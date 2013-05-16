6a099204aa280f80b6ae46a45c4587c2855dd801
Damon Oehlman <damon.oehlman@sidelab.com>
2013-05-16 23:05:09 +1000

    Added nested stylus transform

--- test/transforms.js
+++ test/transforms.js
@@ -10,4 +10,10 @@ describe('transform resolution tests', function() {
 
         riggerBrowserify.call(scope, rigger, 'src/require-styl [stylify]');
     });
+
+    it('should be able to browserify using the stylify transform [nested]', function(done) {
+        var scope = comparator(done, path.resolve(__dirname, 'output/require-styl-nested.js'));
+
+        riggerBrowserify.call(scope, rigger, 'src/require-styl-nested [stylify]');
+    });
 });
